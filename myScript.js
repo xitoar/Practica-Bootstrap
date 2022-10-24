b0021c48313acaf25f6aa60e0eb1ee253ff00ece
Helios <xitoar@hotmail.com>
2022-10-24 11:35:04 -0300

    ultima revision

--- myScript.js
+++ myScript.js
@@ -1,33 +1,40 @@
 const ticketBoton = document.getElementById("resumen");
-const ticketReset =document.getElementById("borrar");
+const ticketReset = document.getElementById("borrar");
 const ticketResultado = document.getElementById("resultado");
 const ticketCant = document.getElementById("inputCantidad");
 const ticketSelec = document.getElementById("inputState");
 
 
-
-
-function calcular(){
-    switch(ticketSelec.value){
-        case "Estudiante" :
-        
-        ticketResultado.textContent = ticketResultado.textContent + (ticketCant.value * (200 -(200 * 0.8)));
-        break;
-        case "Trainee" :
-        
-        ticketResultado.textContent = ticketResultado.textContent + (200 * ticketCant.value * 0.5);
-        break;
-        case "Junior" :
-        
-        ticketResultado.textContent = ticketResultado.textContent + ( ticketCant.value * (200 -(200 * 0.15)));
-        break;
+function calcular() {
+    switch (ticketSelec.value) {
+        case "Estudiante":
+            ticketResultado.textContent = "Total a Pagar: $" + (ticketCant.value * (200 - (200 * 0.8)));
+            break;
+        case "Trainee":
+            ticketResultado.textContent = "Total a Pagar: $" + (200 * ticketCant.value * 0.5);
+            break;
+        case "Junior":
+            ticketResultado.textContent = "Total a Pagar: $" + (ticketCant.value * (200 - (200 * 0.15)));
+            break;
     }
 }
 
-function borrar(){
+function borrar() {
     ticketResultado.textContent = "Total a Pagar: $";
 }
 
-
 ticketBoton.addEventListener("click", calcular);
 ticketReset.addEventListener("click", borrar)
+
+var forms = document.querySelectorAll('.needs-validation')
+
+Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        ticketBoton.addEventListener("click", function (event) {
+            if (!form.checkValidity()) {
+                alert("Verifique los datos cargados")
+            }
+            form.classList.add('was-validated')
+            calcular();
+        }, false)
+    })
